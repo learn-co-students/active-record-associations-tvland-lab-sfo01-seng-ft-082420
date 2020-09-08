c8f261a899ca2cb5ae0e0913d1f73fad72c0242a
Will-Gon <will.colocho@gmail.com>
2020-09-08 13:12:03 -0700

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.characters.map{|c| "#{c.name} - #{c.show.name}"}
+    # actor = Actor.new(:first_name, :last_name)
+    # character = Character.new(:name)
+    # show = Show.new(:name)
+    # characters.show = show
+    # characters.save
+  end
 end
